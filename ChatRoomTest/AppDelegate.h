03883f99583bd91d2f50b365393ab3e2b591ac3f
Gaoyuan Chen <289312802@qq.com>
2015-06-22 10:54:36 -0500

    Initial Commit

---
+++ ChatRoomTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatRoomTest
+//
+//  Created by Gaoyuan Chen on 6/22/15.
+//  Copyright (c) 2015 Gaoyuan Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
